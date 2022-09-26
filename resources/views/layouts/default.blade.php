1dbc6fbdc6f3522674710bd1d0ccbc98c788f5a9
Keisuke Kimura <kim.ksuke@gmail.com>
2022-09-27 08:39:44 +0900

    ログイン画面修正

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,11 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-    <link rel="stylesheet" href="/css/tailwind/tailwind.min.css">
+    <link rel="stylesheet" href="{{ asset('/css/tailwind/tailwind.min.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png">
-    <script src="/js/main.js"></script>
+    <script src="{{ asset('/js/main.js')}}"></script>
 </head>
 <body class="antialiased bg-body text-body font-body">
 
