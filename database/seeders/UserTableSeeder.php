5199dc6a4697b5b6412168659c20c01724791780
Keisuke Kimura <kim.ksuke@gmail.com>
2022-09-27 21:29:47 +0900

    ユーザーデータのシーダーを変更

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -19,9 +19,26 @@ class UserTableSeeder extends Seeder
         $password = '12345678';
         $password = Hash::make($password);
         $param =[
-            'name' => '管理者',
+            'last_name' => '管理者',
+            'last_name_kana' => '１号',
+            'first_name' => 'カンリシャ',
+            'first_name_kana' => 'イチゴウ',
             'email' => '000@gmail.com' ,
             'phone' => '0120-000-000' ,
+            'password' => $password,
+            'default_salon' => 1,
+        ];
+        DB::table('users') -> insert($param);
+        
+        $password = '12345678';
+        $password = Hash::make($password);
+        $param =[
+            'last_name' => 'テストユーザー',
+            'last_name_kana' => '２号',
+            'first_name' => 'テストユーザー',
+            'first_name_kana' => 'ニ「ゴウ',
+            'email' => '222@gmail.com' ,
+            'phone' => '0120-222-222' ,
             'password' => $password,
             'default_salon' => 2,
         ];
@@ -30,22 +47,13 @@ class UserTableSeeder extends Seeder
         $password = '12345678';
         $password = Hash::make($password);
         $param =[
-            'name' => 'テストユーザー２',
-            'email' => '111@gmail.com' ,
-            'phone' => '0120-222-222' ,
-            'password' => $password,
-            'default_salon' => 2,            
-        ];
-        DB::table('users') -> insert($param);
-        
-        $password = '12345678';
-        $password = Hash::make($password);
-        $param =[
-            'name' => 'テストユーザー３号',
+            'last_name' => 'テストユーザー',
+            'last_name_kana' => '３号',
+            'first_name' => 'テストユーザー',
+            'first_name_kana' => 'サンゴウ',
             'email' => '333@gmail.com' ,
             'phone' => '0120-333-333' ,
             'password' => $password,
-
             'default_salon' => 3,
         ];
         DB::table('users') -> insert($param);
@@ -53,12 +61,14 @@ class UserTableSeeder extends Seeder
         $password = '12345678';
         $password = Hash::make($password);
         $param =[
-            'name' => 'テストユーザー4号',
+            'last_name' => 'テストユーザー',
+            'last_name_kana' => '4号',
+            'first_name' => 'テストユーザー',
+            'first_name_kana' => 'ヨンゴウ',
             'email' => '444@gmail.com' ,
             'phone' => '0120-444-444' ,
             'password' => $password,
-
-            'default_salon' => 4,
+            'default_salon' => 3,
         ];
         DB::table('users') -> insert($param);
 
