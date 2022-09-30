cac93eeb2039662ee7a76ccd479ce80697865394
Keisuke Kimura <kim.ksuke@gmail.com>
2022-09-30 15:53:12 +0900

    サロンに連結

--- app/Models/DefaultCapacity.php
+++ app/Models/DefaultCapacity.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class DefaultCapacity extends Model
 {
     use HasFactory;
+
+    public function salon(){
+        return $this -> belongsTo('App\Models\Salon');
+    }
 }
