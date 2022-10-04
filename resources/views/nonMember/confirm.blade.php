2b2ad64e8718309e58ea0d4e7195a4b398c903f7
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-04 22:53:59 +0900

    非会員のための確認フォーム　変数部分をコメントアウト

--- resources/views/nonMember/confirm.blade.php
+++ resources/views/nonMember/confirm.blade.php
@@ -4,12 +4,13 @@
 
 @section('content')
 <div class="container">
-    <p>{{$owner -> getFullName()}}様</p>
-    <p>{{$pet -> getData()}}</p>
-    <p>{{$course -> getCourseInfo()}}</p>
-    <p>{{$date}}</p>
-    <p>{{$timeStr}}</p>
-
+    {{--
+        <p>{{$owner -> getFullName()}}様</p>
+        <p>{{$pet -> getData()}}</p>
+        <p>{{$course -> getCourseInfo()}}</p>
+        <p>{{$date}}</p>
+        <p>{{$timeStr}}</p>
+        --}}
     <p>上記下の内容で予約を行いますか？</p>
 
     <form action="{{Route('booking.store')}}" method="post">
