a0616af8d4a5f50dfa9db523bae21b64e352dfe3
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-04 17:04:33 +0900

    非会員のテーブル設計

--- database/migrations/2022_10_04_165724_create_non_member_booking_table.php
+++ database/migrations/2022_10_04_165724_create_non_member_booking_table.php
@@ -15,6 +15,22 @@ return new class extends Migration
     {
         Schema::create('non_member_booking', function (Blueprint $table) {
             $table->id();
+            $table->integer('booking_id');
+            $table->string('last_name');
+            $table->string('last_name_kana');
+            $table->string('first_name');
+            $table->string('first_name_kana');
+            $table->string('email');
+            $table->string('phone');
+            $table->foreignId('dogtype_id');
+            $table->string('name');
+            $table->date('date');
+            $table->integer('st_time');
+            $table->integer('ed_time');
+            $table->foreignId('course_id');
+            $table->foreignId('salon_id');
+            $table->integer('price');
+            $table->integer('booking_status');
             $table->timestamps();
         });
     }
