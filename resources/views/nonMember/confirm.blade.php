bc71538825fa1d3b9a1cb1c4420a431212b84ec9
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-04 22:47:34 +0900

    非会員の予約確認画面

---
+++ resources/views/nonMember/confirm.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.user')
+
+@section('title', '予約内容の確認')
+
+@section('content')
+<div class="container">
+    <p>{{$owner -> getFullName()}}様</p>
+    <p>{{$pet -> getData()}}</p>
+    <p>{{$course -> getCourseInfo()}}</p>
+    <p>{{$date}}</p>
+    <p>{{$timeStr}}</p>
+
+    <p>上記下の内容で予約を行いますか？</p>
+
+    <form action="{{Route('booking.store')}}" method="post">
+        @csrf
+        <input type="submit" name="" id="" value="予約する">
+    </form>
+</div>
+@endsection
+
+@section('footer')
+@endsection
