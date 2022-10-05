50c12df4d987cf94fc50a53189c00182c678d8cb
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-05 19:24:28 +0900

    Utilの読み込みを修正

--- app/Mail/ContactAdminMail.php
+++ app/Mail/ContactAdminMail.php
@@ -6,7 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use app\classes\Util;
+use App\classes\Util;
 
 class ContactAdminMail extends Mailable
 {
