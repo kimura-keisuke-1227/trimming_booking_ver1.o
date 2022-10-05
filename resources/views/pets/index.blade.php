f45e6b454b641a29c925ec9853b748e1ccfb687c
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-05 20:59:13 +0900

    ペット一覧のスマホ表示を分ける

--- resources/views/pets/index.blade.php
+++ resources/views/pets/index.blade.php
@@ -5,12 +5,12 @@
 @section('content')
 
 <div class="container">
-    <p>こんにちは、{{$owner -> getFullName()}}さん</p>
-    <table class="table table-striped">
+    <p>こんにちは、<br class="sp_only">{{$owner -> getFullName()}}さん</p>
+    <table class="table table-striped pc_only">
         <tr>
-            <td>犬種</td>
-            <td>名前</td>
-            <td>生年月日</td>
+            <th>犬種</th>
+            <th>名前</th>
+            <th>生年月日</th>
         </tr>
         @foreach($pets as $pet)
         <tr>
@@ -19,7 +19,20 @@
             <td>{{$pet -> birthday}}</td>
         </tr>
         @endforeach
-        
+
+    </table>
+
+    <table class="table table-striped sp_only">
+        <tr>
+            <th>登録済みペット一覧</th>
+        </tr>
+        @foreach($pets as $pet)
+        <tr><td>
+            {{$pet -> dogtype -> type}} <br>
+            {{$pet -> getData()}} <br>
+            {{$pet -> birthday}} <br>
+        </td></tr>
+        @endforeach
     </table>
     <a href="/dog_register">ペット登録へ</a>
 
