191dcf1deba9168561105c5c36889c49269e28ef
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-05 10:38:54 +0900

    管理者からの削除用画面をコピー

---
+++ resources/views/admin/bookings/deleteConfirm.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.user')
+
+@section('title', '予約の取消')
+
+@section('content')
+<div class="container">
+    <p>{{$owner -> getFullName()}}様</p>
+    <p>{{$pet -> getData()}}</p>
+    <p>{{$course -> getCourseInfo()}}</p>
+    <p>{{$date}}</p>
+    <p>{{$timeStr}}</p>
+
+    <p>上記の予約を取り消しますか？</p>
+
+    <form action="{{Route('booking.store')}}" method="post">
+        @csrf
+        <input type="submit" name="" id="" value="予約する">
+    </form>
+</div>
+@endsection
+
+@section('footer')
+@endsection
