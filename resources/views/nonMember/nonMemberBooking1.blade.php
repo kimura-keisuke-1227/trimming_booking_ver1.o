32f320f9bbd232b71c8ef5f17d4b26e5a5574bae
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-08 22:34:13 +0900

    エラーメッセージの表示位置修正

--- resources/views/nonMember/nonMemberBooking1.blade.php
+++ resources/views/nonMember/nonMemberBooking1.blade.php
@@ -6,6 +6,17 @@
 <section class="py-8">
     <div class="container px-4 mx-auto">
         <div class="py-4 bg-white rounded">
+            <!-- ▼▼▼▼エラーメッセージ▼▼▼▼　-->
+            @if($errors->any())
+            <div class="mb-8 py-4 px-6 border border-red-300 bg-red-50 rounded">
+                <ul>
+                    @foreach($errors->all() as $error)
+                    <li class="text-red-400">{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+            <!-- ▲▲▲▲エラーメッセージ▲▲▲▲　-->
             <form action="{{Route('nonMember.beginBookingEntry')}}" method="post" enctype="multipart/form-data">
                 @csrf
                 <div class="flex px-6 pb-4 border-b">
@@ -42,17 +53,7 @@
                 </div>
 
                 <div class="pt-4 px-6">
-                    <!-- ▼▼▼▼エラーメッセージ▼▼▼▼　-->
-                    @if($errors->any())
-                    <div class="mb-8 py-4 px-6 border border-red-300 bg-red-50 rounded">
-                        <ul>
-                            @foreach($errors->all() as $error)
-                            <li class="text-red-400">{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                    @endif
-                    <!-- ▲▲▲▲エラーメッセージ▲▲▲▲　-->
+
 
                     <div class="mb-6">
                         <label class="block text-sm font-medium mb-2" for="name">犬種を選択してください</label>
