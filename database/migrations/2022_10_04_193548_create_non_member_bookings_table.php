ec0ba301732e53e67b01ef2b199ec0f28d3c0d6d
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-09 12:15:03 +0900

    非会員にもペットの体重を登録させる

--- database/migrations/2022_10_04_193548_create_non_member_bookings_table.php
+++ database/migrations/2022_10_04_193548_create_non_member_bookings_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->string('email');
             $table->string('phone');
             $table->string('name');
+            $table->float('weight');
             $table->timestamps();
         });
     }
