0a106b1a07bec7ce9d16a4735bb367f0866e2f2d
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-10 06:53:03 +0900

    コメントの入力欄を追加

--- resources/views/bookings/selectcourse.blade.php
+++ resources/views/bookings/selectcourse.blade.php
@@ -50,6 +50,10 @@
                             </div>
                         </div>
                     </div>
+                    <div class="mb-6">
+                        <label class="block text-sm font-medium mb-2" for="message">メッセージ</label>
+                        <textarea id="body" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded" name="body" rows="5"></textarea>
+                    </div>
                     <br>
                     <br>
 
