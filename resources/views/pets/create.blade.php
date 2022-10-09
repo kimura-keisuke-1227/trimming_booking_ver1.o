0455f13d2dc621fbba984b9f38f349bfae5494c8
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-09 11:14:12 +0900

    体重用の入力欄

--- resources/views/pets/create.blade.php
+++ resources/views/pets/create.blade.php
@@ -45,6 +45,10 @@
                         <label class="block text-sm font-medium mb-2" for="name">名前</label>
                         <input id="name" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded" type="text" name="name" value="{{ old('name') }}">
                     </div>
+                    <div class="mb-6">
+                        <label class="block text-sm font-medium mb-2" for="weight">体重(kg)</label>
+                        <input id="name" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded" type="number" name="weight" value="{{ old('weight') }}">
+                    </div>
 
                     <div class="mb-6">
                         <label class="block text-sm font-medium mb-2" for="name">誕生日</label>
