8ba16ede0de4c879efa300c51b4048d053dd4a35
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-10 12:44:08 +0900

    予約詳細画面管理者用

--- resources/views/admin/bookings/showBookingDetail.blade.php
+++ resources/views/admin/bookings/showBookingDetail.blade.php
@@ -1,10 +1,41 @@
 @extends('layouts.user')
 
-@section('title', '予約の取消')
+@section('title', '予約の詳細')
 
 @section('content')
 <div class="container">
-    <p>{{$booking -> id}}</p>
+    <table class="table table-striped">
+        <tr>
+            <td>予約ID</td>
+            <td>{{$booking -> id}}</td>
+        </tr>
+        <tr>
+            <td>ペット</td>
+            <td>{{$booking -> getBookingInfoForStaff()}}</td>
+        </tr>
+        <tr>
+            <td>日付</td>
+            <td>{{$booking -> date}}</td>
+        </tr>
+        <tr>
+            <td>開始時間</td>
+            <td>{{$booking -> getStartTime()}}</td>
+        </tr>
+        <tr>
+            <td>終了時間</td>
+            <td>{{$booking -> getEndTime()}}</td>
+        </tr>
+        <tr>
+            <td>お客様向け終了時間</td>
+            <td>{{$booking -> getEndTimeForOwner()}}</td>
+        </tr>
+        <tr>
+            <td>メッセージ</td>
+            <td>{{$booking -> getBookingMessage()}}</td>
+        </tr>
+    </table>
+
+    <br>
 
     <button type="button" onClick="history.back()">戻る</button>
 
