e97e5c22e077606f906d3ade5365437edf95b2b9
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-12 00:01:54 +0900

    クラスの調整

--- app/Mail/BookingNotificationForSalon.php
+++ app/Mail/BookingNotificationForSalon.php
@@ -7,6 +7,10 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
 class BookingNotificationForSalon extends Mailable
 {
     use Queueable, SerializesModels;
@@ -28,9 +32,35 @@ class BookingNotificationForSalon extends Mailable
      */
     public function build()
     {
+        $user = Auth::user();
+        $date = session('date');
+        $st_time = session('time');
+        $cut_time = session('course')->minute;
+        $cut_time_for_show = session('course')->minute_for_show;
+        $ed_time = $st_time + $cut_time;
+        $ed_time_for_show = $st_time + $cut_time_for_show;
+        $pet = session('pet');
+        $course = session('course');
+        $price =  session('course')->price;
+        $salon = session('salon')->id;
+        $booking_status = 1;
+        $message = session('message');
+
+        Log::debug(__METHOD__ . ' message：' . $message);
+
         $salon = session('salon');
+
         return $this->from($salon->email) 
         ->subject('予約がありました。')
-        ->text('view.name');
+        ->text('email.bookingNotification.bookingNotification',[
+            'user' => $user,
+            'pet' => $pet,
+            'salon' => $salon,
+            'course' => $course,
+            'message_text' => $message,
+            'date' => $date,
+            'st_time' => $st_time,
+            'ed_time_for_show' => $ed_time_for_show,
+        ]);
     }
 }
