1cf2c7ab2e695ad2b3dea154c40b54045f79ccac
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-11 23:13:50 +0900

    サロン向け通知メール

---
+++ app/Mail/BookingNotificationForSalon.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class BookingNotificationForSalon extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $salon = session('salon');
+        return $this->from($salon->email) 
+        ->subject('予約がありました。')
+        ->text('view.name');
+    }
+}
