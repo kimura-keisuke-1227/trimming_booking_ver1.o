a9f1149f7f224b53956c60daa4322ca8f81b48fa
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-12 09:53:33 +0900

    削除用の確認画面を修正

--- resources/views/admin/bookings/deleteConfirm.blade.php
+++ resources/views/admin/bookings/deleteConfirm.blade.php
@@ -5,17 +5,43 @@
 @section('content')
 <div class="container">
 <table class="table table-striped">
+<table class="table table-striped">
         <tr>
             <td>予約ID</td>
             <td>{{$booking -> id}}</td>
         </tr>
         <tr>
+            <td>飼い主様</td>
+            @if($booking -> pet_id ==0)
+
+            <td>{{$booking -> getNonMemberOwner() }}</td>
+            @else
+            <td>{{$booking -> getMemberName()}}</td>
+
+            @endif
+        <tr>
             <td>ペット</td>
+            @if($booking -> pet_id ==0)
+            
+            <td>{{$booking -> getPetNameOfNoMemberBooking() }}</td>
+            @else
             <td>{{$booking -> getBookingInfoForStaff()}}</td>
+
+            @endif
+        </tr>
+        <tr>
+            <td>コース</td>
+            @if($booking -> pet_id ==0)
+            
+            <td>{{$booking -> getDogTypeAndCourse() }}</td>
+            @else
+            <td>{{$booking -> getBookingCourseAndDogTypeInfoForStaff()}}</td>
+
+            @endif
         </tr>
         <tr>
             <td>日付</td>
-            <td>{{$booking -> date}}</td>
+            <td>{{$booking -> getBookingDate()}}</td>
         </tr>
         <tr>
             <td>開始時間</td>
