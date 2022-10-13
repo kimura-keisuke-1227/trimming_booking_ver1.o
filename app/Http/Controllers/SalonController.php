785767cd1f140d0eb76fff5697037a466fb152af
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-13 16:56:04 +0900

    サロン情報を出力するための関数

--- app/Http/Controllers/SalonController.php
+++ app/Http/Controllers/SalonController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Salon;
 
 class SalonController extends Controller
 {
     //
+
+    public function index(){
+
+    }
+
+    public function show($salon_id){
+        $salon = Salon::find($salon_id)->get();
+
+        
+    }
 }
