86c9862971907b58a87cd674a58e773b8d131da4
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-13 11:58:12 +0900

    ユーザー情報のタイトルを変更

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -84,7 +84,7 @@
                     </div>
                     
                     <div class="mb-6">
-                        <label class="block text-sm font-medium mb-2" for="email">過去に当店の利用がありますか？</label>
+                        <label class="block text-sm font-medium mb-2" for="email">利用歴有無</label>
                         <input id="last_name_kana" class="block w-half px-4 py-3 mb-2 text-sm bg-white border rounded" type="text" name="last_name_kana" value="{{ $cameBefore }}">
                     </div>
                     <div class="mb-6">
