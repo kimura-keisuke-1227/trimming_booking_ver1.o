78d83a3f8ba249719fdd77d3240276803c5fe7e2
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-15 00:24:08 +0900

    非会員向けメールにUtil

--- app/Mail/NonMemberBookingMail.php
+++ app/Mail/NonMemberBookingMail.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use App\classes\Util;
 
 class NonMemberBookingMail extends Mailable
 {
@@ -32,13 +33,21 @@ class NonMemberBookingMail extends Mailable
 
         $salon = session('salon');
         $booking = session('booking');
+        $st_time = Util::minuteToTime($booking->st_time);
+        $ed_time = Util::minuteToTime($booking->ed_time);
+        $ed_time_for_show = Util::minuteToTime($booking->ed_time_for_show);
+        $date = Util::getYMDWFromDbDate($booking->date);
 
         return $this->from($salon->email) 
         ->subject('予約を受付けました。')
         ->text('email.bookingNotification.bookingNotificationFromNoMember',[
             'nonMemberBooking' => $nonMemberBooking,
             'salon' => $salon,
-            'booking' => $booking
+            'booking' => $booking,
+            'st_time' => $st_time,
+            'ed_time' => $ed_time,
+            'ed_time_for_show' => $ed_time_for_show,
+            'date' => $date
         ]);
     }
 }
