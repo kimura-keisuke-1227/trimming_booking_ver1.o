76bdc544d0b327ae61a5365e2b8828cdff4809ce
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-14 15:12:42 +0900

    ログインフォームの文言を変更

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -60,7 +60,7 @@
         <div class="flex max-w-md mx-auto flex-col text-center">
           <div class="mt-12 mb-8 p-8 bg-white rounded shadow">
             <h1 class="mb-6 text-3xl">con affetto<br>予約システム</h1>
-            <p>以下にメールアドレス・パスワードを入力して<br>ログインしてください。</p>
+            <p>メールアドレス・パスワードを入力して<br>ログインしてください。</p>
             <br>
             @if($errors->any())
             <div class="mb-8 py-4 px-6 border border-red-300 bg-red-50 rounded">
