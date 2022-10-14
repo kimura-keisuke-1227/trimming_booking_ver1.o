0e603bcc5030f63e3d53e757697aee9873e7e697
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-14 15:40:19 +0900

    予約キャンセルメール

--- app/Mail/CancelMailToUser.php
+++ app/Mail/CancelMailToUser.php
@@ -7,6 +7,12 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
+use App\classes\Util;
+
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
 class CancelMailToUser extends Mailable
 {
     use Queueable, SerializesModels;
@@ -28,6 +34,37 @@ class CancelMailToUser extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        $user = Auth::user();
+        $date = session('date');
+        $st_time = session('time');
+        $cut_time = session('course')->minute;
+        $cut_time_for_show = session('course')->minute_for_show;
+        $ed_time = $st_time + $cut_time;
+        $ed_time_for_show = $st_time + $cut_time_for_show;
+        $pet = session('pet');
+        $course = session('course');
+        $price =  session('course')->price;
+        $salon = session('salon')->id;
+        $booking_status = 1;
+        $message = session('message');
+
+        Log::debug(__METHOD__ . ' message：' . $message);
+
+        $salon = session('salon');
+
+        $mailFrom = 'support@conaffetto-saitama.com';
+
+        return $this->from($mailFrom) 
+        ->subject('予約をキャンセルしました。')
+        ->text('email.bookingNotificationToStaff.bookingNotificationToStaff',[
+            'user' => $user,
+            'pet' => $pet,
+            'salon' => $salon,
+            'course' => $course,
+            'message_text' => $message,
+            'date' => Util::dbDateToStrDate($date),
+            'st_time' => Util::minuteToTime($st_time),
+            'ed_time_for_show' => Util::minuteToTime($ed_time_for_show),
+        ]);
     }
 }
