cb0f020a8718e243568e4773d714161791c2f5ae
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-15 00:13:08 +0900

    bladeの中で渡すのが面倒なので先に作る

--- app/Mail/NonMemberBookingMailToSalon.php
+++ app/Mail/NonMemberBookingMailToSalon.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use App\classes\Util;
 
 class NonMemberBookingMailToSalon extends Mailable
 {
@@ -32,13 +33,19 @@ class NonMemberBookingMailToSalon extends Mailable
 
         $salon = session('salon');
         $booking = session('booking');
+        $st_time = Util::minuteToTime($booking->st_time);
+        $ed_time = Util::minuteToTime($booking->ed_time);
+        $ed_time_for_show = Util::minuteToTime($booking->ed_time_for_show);
 
         return $this->from($salon->email) 
         ->subject('会員でないユーザーから予約がありました。')
-        ->text('email.bookingNotification.bookingNotificationFromNoMember',[
+        ->text('email.bookingNotificationToSalon.bookingNotificationFromNoMember',[
             'nonMemberBooking' => $nonMemberBooking,
             'salon' => $salon,
-            'booking' => $booking
+            'booking' => $booking,
+            'st_time' => $st_time,
+            'ed_time' => $ed_time,
+            'ed_time_for_show' => $ed_time_for_show,
         ]);
     }
 }
