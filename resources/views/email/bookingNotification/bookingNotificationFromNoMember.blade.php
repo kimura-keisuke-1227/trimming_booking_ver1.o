db7851ffa5954074861a7eec3c5c801b0df8eb69
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-14 23:54:39 +0900

    非会員のためのメール文

---
+++ resources/views/email/bookingNotification/bookingNotificationFromNoMember.blade.php
@@ -0,0 +1,21 @@
+{{ $nonMemberBooking->last_name .$nonMemberBooking ->first_name }} 様
+
+以下の通り、予約を受付けました。
+
+【予約内容】
+ペットのお名前:{{ $nonMemberBooking -> name}}
+犬種:{{ $booking -> course -> dogtype -> type}}
+予約店舗: {{ $salon -> salon_name }}
+コース: {{ $booking-> course -> courseMaster -> course}}
+予約日：{{ $booking -> date }}
+開始時刻：{{ $booking -> st_time }}
+終了予定時刻：{{ $booking -> ed_time_for_show }}
+メッセージ:
+{{ $booking -> message }}
+
+(ご注意)
+約の状況に応じて店舗よりお客様にお電話をさせていただくことがあります。
+また、来店時に狂犬病と混合ワクチン接種証明書をご持参ください。
+
+※このメールは配信専用のアドレスで配信されています。
+このメールに返信されても返信内容の確認およびご返答ができませんので、ご了承ください。
