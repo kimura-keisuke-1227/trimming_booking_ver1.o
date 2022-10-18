1037cdd6fbe46524891ed1325b87d25e51de2496
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-18 22:11:35 +0900

    日付を指定することで、全ユーザーの来店歴を取得する関数が作れた

--- app/classes/Util.php
+++ app/classes/Util.php
@@ -1,57 +1,69 @@
 <?php
+
 namespace app\classes;
+
 use Illuminate\Support\Facades\Log;
 use App\Models\Setting;
+use App\Models\User;
+use App\Models\Booking;
+use App\Models\Pet;
 use Illuminate\Support\Facades\Auth;
 use App\Models\OpenCloseSalon;
 use Illuminate\Support\Facades\DB;
 
 class Util
 {
-    public static function getYMDWFromDbDate($date){
-        $dateStr = date('Y年m月d日',strtotime($date));
-        $week = array( "日", "月", "火", "水", "木", "金", "土" );
+    public static function getYMDWFromDbDate($date)
+    {
+        $dateStr = date('Y年m月d日', strtotime($date));
+        $week = array("日", "月", "火", "水", "木", "金", "土");
 
-        $dateStr = $dateStr . '('. $week[ date('w',strtotime($date))] . ')';
+        $dateStr = $dateStr . '(' . $week[date('w', strtotime($date))] . ')';
         return $dateStr;
     }
 
-    public static function dbDateToStrDate($date){
-        $week = array( "日", "月", "火", "水", "木", "金", "土" );
-        $dateStr = date('m/d',strtotime($date)) .'('. $week[ date('w',strtotime($date))] . ')';
+    public static function dbDateToStrDate($date)
+    {
+        $week = array("日", "月", "火", "水", "木", "金", "土");
+        $dateStr = date('m/d', strtotime($date)) . '(' . $week[date('w', strtotime($date))] . ')';
 
         return $dateStr;
     }
-    public static function dbDateToStrDateForBirthday($date){
-        $dateStr = date('Y年m月d日',strtotime($date));
+    public static function dbDateToStrDateForBirthday($date)
+    {
+        $dateStr = date('Y年m月d日', strtotime($date));
 
         return $dateStr;
     }
 
-    public static function minuteToTime($timeNum){
+    public static function minuteToTime($timeNum)
+    {
         $hour = (string) floor($timeNum / 60);
         $minute = (string) ($timeNum % 60);
 
         return $hour . '時' . $minute . '分';
     }
 
-    public static function minuteNumToTime($time){
+    public static function minuteNumToTime($time)
+    {
         $hour = (string) floor($time / 60);
         $minute = (string) $time % 60;
 
         return $hour . '時' . $minute . '分';
     }
 
-    public static function timeToMinute($hour,$minute){
+    public static function timeToMinute($hour, $minute)
+    {
         return $hour * 60 + $minute;
     }
 
-    public static function addDays($baseDay, $addDays){
-        
-        $time = '+' . $addDays .'day';
-        return date('Y-m-d',strtotime($time,strtotime($baseDay)));
+    public static function addDays($baseDay, $addDays)
+    {
+
+        $time = '+' . $addDays . 'day';
+        return date('Y-m-d', strtotime($time, strtotime($baseDay)));
     }
-    
+
     /*
     public static function dateFormat($date){
         Log::debug(__FUNCTION__ . ' $date(m):' );
@@ -61,63 +73,67 @@ class Util
     }
     */
 
-    public static function dateFormat($date){
-        $week = array( "日", "月", "火", "水", "木", "金", "土" );
-        $dateStr = date('m/d',strtotime($date)) .'('. $week[ date('w',strtotime($date))] . ')';
+    public static function dateFormat($date)
+    {
+        $week = array("日", "月", "火", "水", "木", "金", "土");
+        $dateStr = date('m/d', strtotime($date)) . '(' . $week[date('w', strtotime($date))] . ')';
 
         return $dateStr;
     }
-    
+
     public static function getSetting($default, $setting_name, $isInt)
-    {   
+    {
         $user = Auth::user();
 
-        Log::debug(__METHOD__.'('.__LINE__.') start!');
-        $setting = Setting::where('setting_name',$setting_name) -> get();
+        Log::debug(__METHOD__ . '(' . __LINE__ . ') start!');
+        $setting = Setting::where('setting_name', $setting_name)->get();
 
-        if(is_null($user)){
+        if (is_null($user)) {
             $reader = 'Non Member User';
-        } else{
+        } else {
             $reader = $user->id;
         }
 
-        Log::info(__FUNCTION__ .' user('. $reader . ') get setting '. $setting_name. ' (' . $setting .')');
-        
-        
+        Log::info(__FUNCTION__ . ' user(' . $reader . ') get setting ' . $setting_name . ' (' . $setting . ')');
+
+
         $settingHere = $setting;
-        
+
         //設定がなかった場合
-        Log::debug(__METHOD__.'('.__LINE__.') end ');
-        if ($settingHere -> count() == 0) {
+        Log::debug(__METHOD__ . '(' . __LINE__ . ') end ');
+        if ($settingHere->count() == 0) {
             return $default;
         }
 
         if ($isInt) {
-            return $settingHere->first() ->setting_int;
+            return $settingHere->first()->setting_int;
         } else {
-            return $settingHere->first() ->setting_string;
+            return $settingHere->first()->setting_string;
         }
     }
 
-    public function getDaysList($st_date,$ed_date){
+    public function getDaysList($st_date, $ed_date)
+    {
         $days = [];
-        for ($i = $st_date; $i <= $ed_date; $i = $this -> addDays($i, 1)) {
+        for ($i = $st_date; $i <= $ed_date; $i = $this->addDays($i, 1)) {
             $days[$i] = $i;
         }
 
         return $days;
     }
 
-    public function getTimes($st_time,$ed_time,$step_time){
+    public function getTimes($st_time, $ed_time, $step_time)
+    {
         $times = [];
         for ($time = $st_time; $time < $ed_time; $time = $time + $step_time) {
             $str_time = Util::minuteToTime($time);
             $times[$time] = $str_time;
-            Log::debug(__METHOD__.'('.__LINE__.')');
+            Log::debug(__METHOD__ . '(' . __LINE__ . ')');
         }
         return $times;
     }
-    public function getTimesNum($st_time,$ed_time,$step_time){
+    public function getTimesNum($st_time, $ed_time, $step_time)
+    {
         $timesNum = [];
         for ($time = $st_time; $time < $ed_time; $time = $time + $step_time) {
             $str_time = Util::minuteToTime($time);
@@ -126,7 +142,8 @@ class Util
         }
         return $timesNum;
     }
-    public static function getAge($date, $birthday){
+    public static function getAge($date, $birthday)
+    {
         $birthdayYear = date('Y', strtotime($birthday));
         $birthdayMonth = date('m', strtotime($birthday));
         $birthdayDay = date('d', strtotime($birthday));
@@ -135,37 +152,40 @@ class Util
         $month = date('m', strtotime($date));
         $day = date('d', strtotime($date));
 
-        $age =  $year - $birthdayYear-1;
+        $age =  $year - $birthdayYear - 1;
 
-        $birth_month_year = $birthdayMonth*100 + $birthdayDay;
-        $month_year = $month*100 + $day;
+        $birth_month_year = $birthdayMonth * 100 + $birthdayDay;
+        $month_year = $month * 100 + $day;
 
-        if($month_year >= $birth_month_year){
-            $age  = $age+1;
+        if ($month_year >= $birth_month_year) {
+            $age  = $age + 1;
         }
 
         return (string) $age . '歳';
     }
 
-    public static function getEndOfTheMonth($date, $addMonth){
+    public static function getEndOfTheMonth($date, $addMonth)
+    {
 
-        
-        return date("Y-m-t", strtotime($date. (string) $addMonth ."month"));
+
+        return date("Y-m-t", strtotime($date . (string) $addMonth . "month"));
     }
 
-    public function saveAccessLog($user_id,$method,$message){
-        Log::debug(__METHOD__.'('.__LINE__.') start!');
-        Log::debug(__METHOD__.'('.__LINE__.') end!');
+    public function saveAccessLog($user_id, $method, $message)
+    {
+        Log::debug(__METHOD__ . '(' . __LINE__ . ') start!');
+        Log::debug(__METHOD__ . '(' . __LINE__ . ') end!');
     }
-    
-    public function closeBooked($salon_id, $date,$st_time,$ed_time,$course_id){
-        Log::debug(__METHOD__.'('.__LINE__.') start!');
 
-        $step_time = $this->getSetting(30,'step_time',true);
+    public function closeBooked($salon_id, $date, $st_time, $ed_time, $course_id)
+    {
+        Log::debug(__METHOD__ . '(' . __LINE__ . ') start!');
+
+        $step_time = $this->getSetting(30, 'step_time', true);
 
         $insertsDatas = [];
-        for($time = $st_time ; $time<$ed_time; $time = $time + $step_time){
-            $openCloseSalon=[
+        for ($time = $st_time; $time < $ed_time; $time = $time + $step_time) {
+            $openCloseSalon = [
                 'salon_id' => $salon_id,
                 'course_id' => $course_id,
                 'date' => $date,
@@ -178,11 +198,36 @@ class Util
             $insertsDatas[] = $openCloseSalon;
         }
 
-        DB::table('open_close_salons') -> insert($insertsDatas);
-        Log::debug(__METHOD__.'('.__LINE__.') inserted datas!');
+        DB::table('open_close_salons')->insert($insertsDatas);
+        Log::debug(__METHOD__ . '(' . __LINE__ . ') inserted datas!');
         Log::debug($insertsDatas);
-        
 
-        Log::debug(__METHOD__.'('.__LINE__.') end!');
+
+        Log::debug(__METHOD__ . '(' . __LINE__ . ') end!');
+    }
+
+    public function getWhoCameBefore($date)
+    {
+        $users = User::all();
+        $pets = Pet::all();
+        $allBookings = Booking::where('date', '<', $date)->get();
+
+        $usersCameBeforeList = [];
+        foreach($users as $user){
+            $count = $user->cameBefore;
+            $pets_of_user = $pets->where('owner_id',$user->id);
+            foreach($pets_of_user as $pet_of_user){
+                $count = $count + $allBookings->where('pet_id',$pet_of_user->id) -> count();
+
+                Log::debug(__METHOD__.'('.__LINE__.') user('. $user->id .') pet_id(' .$pet_of_user->id.') count:'.$count);
+            }
+
+            #Log::debug(__METHOD__.'('.__LINE__.') user('. $user->id .') pets_of_user:' . $pets_of_user);
+            #$bookings_of_user = $allBookings->wherein('pet_id',$pets_of_user->id);
+            #Log::debug(__METHOD__.'('.__LINE__.') user(' . $user->id .') booking:');
+            $usersCameBeforeList[$user->id] = $count;
+        }
+        Log::debug(__METHOD__.'('.__LINE__.') usersCameBeforeList:');
+        Log::debug($usersCameBeforeList);
     }
 }
