eaa381058a494261d0df937b962fd48168e6ff99
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-19 18:10:11 +0900

    ゲスト画面の表示変更

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 
 <head>
-    <title>管理画面</title>
+    <title>ゲスト画面</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap">
