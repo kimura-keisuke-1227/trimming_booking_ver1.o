978a0c5c5c8f178baad1889f2abc0d6da4bfdf43
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-20 12:40:22 +0900

    ボタンを上にもつけた

--- resources/views/admin/openclose/index.blade.php
+++ resources/views/admin/openclose/index.blade.php
@@ -9,26 +9,29 @@
 
     <form action="{{Route('admin.checkOpenCloseWithDate.change')}}" method="get">
         @csrf
+        <div class="flex px-6 pb-4 border-b">
+            <div class="ml-auto">
+                <button type="submit" class="py-2 px-3 text-xs text-white font-semibold bg-indigo-500 rounded-md">登録</button>
+            </div>
+        </div>
         <label for="salon"></label>
         <select name="salon" id="">
             @foreach($salons as $salon)
-            <option value="{{$salon -> id}}" 
-            @if($salon->id == $selectedSalon)
+            <option value="{{$salon -> id}}" @if($salon->id == $selectedSalon)
                 selected
-            @endif
-            >{{$salon -> salon_name}}</option>
+                @endif
+                >{{$salon -> salon_name}}</option>
             @endforeach
         </select>
         <select name="course" id="course">
             @foreach($courses as $course)
-            <option value="{{$course -> id}}" 
-            @if($course_id == $course -> id)
-            selected
-            @php
+            <option value="{{$course -> id}}" @if($course_id==$course -> id)
+                selected
+                @php
                 $displayCourse = $course -> course;
-            @endphp
-            @endif
-            >{{$course -> course}}</option>
+                @endphp
+                @endif
+                >{{$course -> course}}</option>
             @endforeach
         </select>
         <input type="date" name="st_date" value="{{$st_date}}">
@@ -38,9 +41,9 @@
     <br>
 
     <br>
-    <h3>{{$salons->find($selectedSalon)->salon_name}}  
+    <h3>{{$salons->find($selectedSalon)->salon_name}}
         @php
-            echo $displayCourse;
+        echo $displayCourse;
         @endphp
     </h3>
 
@@ -56,12 +59,12 @@
         ])}}">次週へ</a>
 
     @php
-        if($course_id==2){
-            $another_course_id = 1;
-        } else{
-            
-            $another_course_id = 2;
-        }
+    if($course_id==2){
+    $another_course_id = 1;
+    } else{
+
+    $another_course_id = 2;
+    }
 
     @endphp
     <a href="{{Route('admin.checkOpenCloseWithDate', [
@@ -77,39 +80,41 @@
                 @foreach($days as $day)
                 <th class="">
                     @php
-                        $week = array( "日", "月", "火", "水", "木", "金", "土" );
-                        $dateStr = date('m/d',strtotime($day)) .'('. $week[ date('w',strtotime($day))] . ')';
-                        echo $dateStr; 
+                    $week = array( "日", "月", "火", "水", "木", "金", "土" );
+                    $dateStr = date('m/d',strtotime($day)) .'('. $week[ date('w',strtotime($day))] . ')';
+                    echo $dateStr;
                     @endphp
                 </th>
 
                 @endforeach
             </tr>
-            
+
             @foreach($times as $time)
             <tr>
                 <th>{{$time}}</th>
                 @foreach($days as $day)
-                    <!-- <td>{{$capacities[$day][$timesNum[$time]]}}</td> -->
-                    @if($capacities[$day][$timesNum[$time]] == 1)
-                    <td id="td_{{$day}}_{{ $timesNum[$time]}}" class="{{$day}}_{{ $timesNum[$time]}} opened"> <p>○</p>
-                        <input id="opened_{{$day}}_{{$timesNum[$time]}}" type="hidden" class="opened_input" name="{{$day}}_{{$timesNum[$time]}}" id="" value="1">
-                    </td>
-                    @elseif($capacities[$day][$timesNum[$time]] == 2)
-                    <td id="td_{{$day}}_{{ $timesNum[$time]}}"  class="{{$day}}_{{ $timesNum[$time]}} opened"><p>○</p>
-                        <input id="opened_{{$day}}_{{$timesNum[$time]}}" type="hidden"  class="opened_input" name="{{$day}}_{{$timesNum[$time]}}" id="" value="1">
-                        
-                    </td>
-                    @elseif($capacities[$day][$timesNum[$time]] == 0)
-                    <td id="td_{{$day}}_{{ $timesNum[$time]}}"  class="{{$day}}_{{ $timesNum[$time]}} closed">
-                        <p>×</p>
-                        
-                        <input id="closed_{{$day}}_{{$timesNum[$time]}}" class="closed_input" type="hidden" name="{{$day}}_{{$timesNum[$time]}}" id="" value="0">
-                    </td>
-                    @elseif($capacities[$day][$timesNum[$time]] == -1)
-                    <td>定休日</td>
-                    <input id="closed_{{$day}}_{{$timesNum[$time]}}" class="closed_input" type="hidden" name="{{$day}}_{{$timesNum[$time]}}" id="" value="-1">
-                    @endif
+                <!-- <td>{{$capacities[$day][$timesNum[$time]]}}</td> -->
+                @if($capacities[$day][$timesNum[$time]] == 1)
+                <td id="td_{{$day}}_{{ $timesNum[$time]}}" class="{{$day}}_{{ $timesNum[$time]}} opened">
+                    <p>○</p>
+                    <input id="opened_{{$day}}_{{$timesNum[$time]}}" type="hidden" class="opened_input" name="{{$day}}_{{$timesNum[$time]}}" id="" value="1">
+                </td>
+                @elseif($capacities[$day][$timesNum[$time]] == 2)
+                <td id="td_{{$day}}_{{ $timesNum[$time]}}" class="{{$day}}_{{ $timesNum[$time]}} opened">
+                    <p>○</p>
+                    <input id="opened_{{$day}}_{{$timesNum[$time]}}" type="hidden" class="opened_input" name="{{$day}}_{{$timesNum[$time]}}" id="" value="1">
+
+                </td>
+                @elseif($capacities[$day][$timesNum[$time]] == 0)
+                <td id="td_{{$day}}_{{ $timesNum[$time]}}" class="{{$day}}_{{ $timesNum[$time]}} closed">
+                    <p>×</p>
+
+                    <input id="closed_{{$day}}_{{$timesNum[$time]}}" class="closed_input" type="hidden" name="{{$day}}_{{$timesNum[$time]}}" id="" value="0">
+                </td>
+                @elseif($capacities[$day][$timesNum[$time]] == -1)
+                <td>定休日</td>
+                <input id="closed_{{$day}}_{{$timesNum[$time]}}" class="closed_input" type="hidden" name="{{$day}}_{{$timesNum[$time]}}" id="" value="-1">
+                @endif
                 @endforeach
             </tr>
             @endforeach
@@ -121,24 +126,24 @@
         <input type="hidden" name="salon_id" value="{{$selectedSalon}}">
         <input type="hidden" name="course_id" value="{{$course_id}}">
         <div class="flex px-6 pb-4 border-b">
-                        <div class="ml-auto">
-                            <button type="submit" class="py-2 px-3 text-xs text-white font-semibold bg-indigo-500 rounded-md">登録</button>
-                        </div>
-                    </div>
+            <div class="ml-auto">
+                <button type="submit" class="py-2 px-3 text-xs text-white font-semibold bg-indigo-500 rounded-md">登録</button>
+            </div>
+        </div>
     </form>
 
     <a href="{{Route('admin.makebooking')}}">新規予約へ</a>
 </div>
 
 <script>
-    $(document).on("click",'.opened', function () {
+    $(document).on("click", '.opened', function() {
         console.log('opened was clicked!');
         $(this).children('p').text("×");
         $(this).children('input').val("0");
         $(this).removeClass('opened');
         $(this).addClass('closed');
     });
-    $(document).on("click",'.closed', function () {
+    $(document).on("click", '.closed', function() {
         console.log('closed was clicked!');
         $(this).children('p').text("○");
         $(this).children('input').val("1");
