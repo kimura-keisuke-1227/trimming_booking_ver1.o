969e871f53185b0b848302795bf6d5c7f37e31f5
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-20 17:00:19 +0900

    定休日を1枠で表示するように

--- resources/views/bookings/booking_calender.blade.php
+++ resources/views/bookings/booking_calender.blade.php
@@ -41,7 +41,9 @@
                 @if ($day <=$maxBookingDate) @if($capacities[$day][$timesNum[$time]]> 0)
                     <td><a href="{{route('booking.selectCalender.date' , ['date' => $day, 'time' => $timesNum[$time]])}}">○</a></td>
                     @elseif($capacities[$day][$timesNum[$time]] == -1)
-                    <td>定休日</td>
+                        @if ($time==$timeOfFirst)
+                            <td rowspan={{$timesCount}}>定休日</td>
+                        @endif
                     @elseif($capacities[$day][$timesNum[$time]] == 0)
                     <td>×</td>
                     @endif
@@ -81,7 +83,9 @@
                 @if ($day <=$maxBookingDate) @if($capacities[$day][$timesNum[$time]]> 0)
                     <td><a href="{{route('booking.selectCalender.date' , ['date' => $day, 'time' => $timesNum[$time]])}}">○</a></td>
                     @elseif($capacities[$day][$timesNum[$time]] == -1)
-                    <td>定休日</td>
+                        @if ($time==$timeOfFirst)
+                            <td rowspan={{$timesCount}}>定休日</td>
+                        @endif
                     @elseif($capacities[$day][$timesNum[$time]] == 0)
                     <td>×</td>
                     @endif
