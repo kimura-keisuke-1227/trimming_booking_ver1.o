8da7473da20ff8fb94a500b0aba57c8fb60b038a
Keisuke Kimura <kim.ksuke@gmail.com>
2022-10-23 00:13:49 +0900

    methodディレクティブを指定

--- resources/views/pets/edit.blade.php
+++ resources/views/pets/edit.blade.php
@@ -6,8 +6,9 @@
 <section class="py-8">
     <div class="container px-4 mx-auto">
         <div class="py-4 bg-white rounded">
-            <form action="{{Route('pets.store')}}" method="post" enctype="multipart/form-data">
+            <form action="{{Route('pets.update',['pet' => $pet])}}" method="POST" enctype="multipart/form-data">
                 @csrf
+                @method('PUT')
                 <div class="flex px-6 pb-4 border-b">
                     <h3 class="text-xl font-bold">ペット情報</h3>
                 </div>
