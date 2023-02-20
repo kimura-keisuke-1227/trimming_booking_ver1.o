75524826ba580a5444faef294b743c0b49524b7c
Keisuke Kimura <kim.ksuke@gmail.com>
2023-02-20 15:52:42 +0900

    予約テストデータ削除

--- database/seeders/BookingsTableSeeder.php
+++ database/seeders/BookingsTableSeeder.php
@@ -16,136 +16,136 @@ class BookingsTableSeeder extends Seeder
     public function run()
     {
         
-        $param = [
-            'id' => 1,
-            'date' => '2022-10-10' ,
-            'st_time' =>  600,
-            'ed_time' =>  720,
-            'ed_time_for_show' =>  720,
-            'pet_id' => 0 ,
-            'course_id' => 2 ,
-            'salon_id' => 2,
-            'price' => 3000 ,
-            'message' => '予約時のメッセージ' ,
-            'booking_status' => 1 ,
-        ];
-        DB::table('bookings') -> insert($param);
+        // $param = [
+        //     'id' => 1,
+        //     'date' => '2022-10-10' ,
+        //     'st_time' =>  600,
+        //     'ed_time' =>  720,
+        //     'ed_time_for_show' =>  720,
+        //     'pet_id' => 0 ,
+        //     'course_id' => 2 ,
+        //     'salon_id' => 2,
+        //     'price' => 3000 ,
+        //     'message' => '予約時のメッセージ' ,
+        //     'booking_status' => 1 ,
+        // ];
+        // DB::table('bookings') -> insert($param);
 
-        $param = [
-            'id' => 2,
-            'date' => '2022-10-10' ,
-            'st_time' =>  600,
-            'ed_time' =>  750,
-            'ed_time_for_show' =>  750,
-            'pet_id' => 0 ,
-            'course_id' => 2 ,
-            'salon_id' => 2,
-            'price' => 3000 ,
-            'booking_status' => 1 ,
-        ];
-        DB::table('bookings') -> insert($param);
+        // $param = [
+        //     'id' => 2,
+        //     'date' => '2022-10-10' ,
+        //     'st_time' =>  600,
+        //     'ed_time' =>  750,
+        //     'ed_time_for_show' =>  750,
+        //     'pet_id' => 0 ,
+        //     'course_id' => 2 ,
+        //     'salon_id' => 2,
+        //     'price' => 3000 ,
+        //     'booking_status' => 1 ,
+        // ];
+        // DB::table('bookings') -> insert($param);
 
-        $param = [
-            'date' => '2022-10-05' ,
-            'st_time' =>  600,
-            'ed_time' =>  720,
-            'ed_time_for_show' =>  720,
-            'pet_id' => 1 ,
-            'course_id' => 2 ,
-            'salon_id' => 2,
-            'price' => 3000 ,
-            'message' => '予約時のメッセージ' ,
-            'booking_status' => 1 ,
-        ];
-        DB::table('bookings') -> insert($param);
+        // $param = [
+        //     'date' => '2022-10-05' ,
+        //     'st_time' =>  600,
+        //     'ed_time' =>  720,
+        //     'ed_time_for_show' =>  720,
+        //     'pet_id' => 1 ,
+        //     'course_id' => 2 ,
+        //     'salon_id' => 2,
+        //     'price' => 3000 ,
+        //     'message' => '予約時のメッセージ' ,
+        //     'booking_status' => 1 ,
+        // ];
+        // DB::table('bookings') -> insert($param);
 
-        $param = [
-            'date' => '2022-10-05' ,
-            'st_time' =>  600,
-            'ed_time' =>  750,
-            'ed_time_for_show' =>  750,
-            'pet_id' => 2 ,
-            'course_id' => 2 ,
-            'salon_id' => 2,
-            'price' => 3000 ,
-            'message' => '予約時のメッセージ' ,
-            'booking_status' => 1 ,
-        ];
-        DB::table('bookings') -> insert($param);
-        $param = [
-            'date' => '2022-10-05' ,
-            'st_time' =>  600,
-            'ed_time' =>  720,
-            'ed_time_for_show' =>  720,
-            'pet_id' => 1 ,
-            'course_id' => 2 ,
-            'salon_id' => 2,
-            'price' => 3000 ,
-            'booking_status' => 1 ,
-        ];
-        DB::table('bookings') -> insert($param);
+        // $param = [
+        //     'date' => '2022-10-05' ,
+        //     'st_time' =>  600,
+        //     'ed_time' =>  750,
+        //     'ed_time_for_show' =>  750,
+        //     'pet_id' => 2 ,
+        //     'course_id' => 2 ,
+        //     'salon_id' => 2,
+        //     'price' => 3000 ,
+        //     'message' => '予約時のメッセージ' ,
+        //     'booking_status' => 1 ,
+        // ];
+        // DB::table('bookings') -> insert($param);
+        // $param = [
+        //     'date' => '2022-10-05' ,
+        //     'st_time' =>  600,
+        //     'ed_time' =>  720,
+        //     'ed_time_for_show' =>  720,
+        //     'pet_id' => 1 ,
+        //     'course_id' => 2 ,
+        //     'salon_id' => 2,
+        //     'price' => 3000 ,
+        //     'booking_status' => 1 ,
+        // ];
+        // DB::table('bookings') -> insert($param);
 
-        $param = [
-            'date' => '2022-10-05' ,
-            'st_time' =>  600,
-            'ed_time' =>  750,
-            'ed_time_for_show' =>  750,
-            'pet_id' => 2 ,
-            'course_id' => 2 ,
-            'salon_id' => 2,
-            'price' => 3000 ,
-            'booking_status' => 1 ,
-        ];
-        DB::table('bookings') -> insert($param);
-        $param = [
-            'date' => '2022-10-05' ,
-            'st_time' =>  600,
-            'ed_time' =>  720,
-            'ed_time_for_show' =>  720,
-            'pet_id' => 1 ,
-            'course_id' => 2 ,
-            'salon_id' => 2,
-            'price' => 3000 ,
-            'booking_status' => 1 ,
-        ];
-        DB::table('bookings') -> insert($param);
+        // $param = [
+        //     'date' => '2022-10-05' ,
+        //     'st_time' =>  600,
+        //     'ed_time' =>  750,
+        //     'ed_time_for_show' =>  750,
+        //     'pet_id' => 2 ,
+        //     'course_id' => 2 ,
+        //     'salon_id' => 2,
+        //     'price' => 3000 ,
+        //     'booking_status' => 1 ,
+        // ];
+        // DB::table('bookings') -> insert($param);
+        // $param = [
+        //     'date' => '2022-10-05' ,
+        //     'st_time' =>  600,
+        //     'ed_time' =>  720,
+        //     'ed_time_for_show' =>  720,
+        //     'pet_id' => 1 ,
+        //     'course_id' => 2 ,
+        //     'salon_id' => 2,
+        //     'price' => 3000 ,
+        //     'booking_status' => 1 ,
+        // ];
+        // DB::table('bookings') -> insert($param);
 
-        $param = [
-            'date' => '2022-10-05' ,
-            'st_time' =>  600,
-            'ed_time' =>  750,
-            'ed_time_for_show' =>  750,
-            'pet_id' => 2 ,
-            'course_id' => 2 ,
-            'salon_id' => 2,
-            'price' => 3000 ,
-            'booking_status' => 1 ,
-        ];
-        DB::table('bookings') -> insert($param);
-        $param = [
-            'date' => '2022-10-05' ,
-            'st_time' =>  600,
-            'ed_time' =>  720,
-            'ed_time_for_show' =>  720,
-            'pet_id' => 1 ,
-            'course_id' => 2 ,
-            'salon_id' => 2,
-            'price' => 3000 ,
-            'booking_status' => 1 ,
-        ];
-        DB::table('bookings') -> insert($param);
+        // $param = [
+        //     'date' => '2022-10-05' ,
+        //     'st_time' =>  600,
+        //     'ed_time' =>  750,
+        //     'ed_time_for_show' =>  750,
+        //     'pet_id' => 2 ,
+        //     'course_id' => 2 ,
+        //     'salon_id' => 2,
+        //     'price' => 3000 ,
+        //     'booking_status' => 1 ,
+        // ];
+        // DB::table('bookings') -> insert($param);
+        // $param = [
+        //     'date' => '2022-10-05' ,
+        //     'st_time' =>  600,
+        //     'ed_time' =>  720,
+        //     'ed_time_for_show' =>  720,
+        //     'pet_id' => 1 ,
+        //     'course_id' => 2 ,
+        //     'salon_id' => 2,
+        //     'price' => 3000 ,
+        //     'booking_status' => 1 ,
+        // ];
+        // DB::table('bookings') -> insert($param);
 
-        $param = [
-            'date' => '2022-10-05' ,
-            'st_time' =>  600,
-            'ed_time' =>  750,
-            'ed_time_for_show' =>  750,
-            'pet_id' => 2 ,
-            'course_id' => 2 ,
-            'salon_id' => 2,
-            'price' => 3000 ,
-            'booking_status' => 1 ,
-        ];
-        DB::table('bookings') -> insert($param);
+        // $param = [
+        //     'date' => '2022-10-05' ,
+        //     'st_time' =>  600,
+        //     'ed_time' =>  750,
+        //     'ed_time_for_show' =>  750,
+        //     'pet_id' => 2 ,
+        //     'course_id' => 2 ,
+        //     'salon_id' => 2,
+        //     'price' => 3000 ,
+        //     'booking_status' => 1 ,
+        // ];
+        // DB::table('bookings') -> insert($param);
     }
 }
