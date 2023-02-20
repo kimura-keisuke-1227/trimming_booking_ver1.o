627ff889c06f100da7dead476b1091db38377a27
Keisuke Kimura <kim.ksuke@gmail.com>
2023-02-20 15:50:29 +0900

    定休日データ削除

--- database/seeders/RegularHolidaysTableSeeder.php
+++ database/seeders/RegularHolidaysTableSeeder.php
@@ -15,18 +15,6 @@ class RegularHolidaysTableSeeder extends Seeder
      */
     public function run()
     {
-        $param =[
-            'salon_id' => 1,
-            'st_date' => '2022-09-01',
-            'dayOfWeek' => 3,
-        ];
-        DB::table('regular_holidays') -> insert($param);
-        $param =[
-            'salon_id' => 2,
-            'st_date' => '2022-09-01',
-            'dayOfWeek' => 4,
-        ];
-        DB::table('regular_holidays') -> insert($param);
 
     }
 }
