a68ab45905f6469000c384f2bbabaa420b197dce
Keisuke Kimura <kim.ksuke@gmail.com>
2023-02-25 10:37:30 +0900

    サロン一覧データを取得し、bladeにわたす

--- app/Http/Controllers/SalonController.php
+++ app/Http/Controllers/SalonController.php
@@ -14,7 +14,11 @@ class SalonController extends Controller
 
     public function index(){
         Log::info(__METHOD__.'('.__LINE__.') start by user(' . Util::getUserId() .')');
+        $salons = Salon::all();
         Log::info(__METHOD__.'('.__LINE__.') end by user(' . Util::getUserId() .')');
+        return view('admin.salons.index', [
+            'salons' => $salons,
+        ]);
     }
 
     public function show($salon_id){
