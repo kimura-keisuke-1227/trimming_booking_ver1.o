1931b524ca57234db4639311929d33fb0da8192b
Keisuke Kimura <kim.ksuke@gmail.com>
2023-02-25 10:57:27 +0900

    サロンの設定を表示させる

--- app/Http/Controllers/SalonController.php
+++ app/Http/Controllers/SalonController.php
@@ -33,8 +33,12 @@ class SalonController extends Controller
     public function edit($salon_id){
         Log::info(__METHOD__.'('.__LINE__.') start by user(' . Util::getUserId() .')');
         $salon = Salon::find($salon_id)->get();
+        Log::debug(__METHOD__.'('.__LINE__.') user(' . Util::getUserId() .')');
+        Log::debug($salon);
         Log::info(__METHOD__.'('.__LINE__.') end by user(' . Util::getUserId() .')');
 
-        
+        return view('admin.salons.edit', [
+            'salon' => $salon,
+        ]);
     }
 }
