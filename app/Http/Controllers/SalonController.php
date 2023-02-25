2b7793d0557a2dd31aafcd09d3a68aae424074dd
Keisuke Kimura <kim.ksuke@gmail.com>
2023-02-25 10:28:59 +0900

    サロンの一覧を表示する

--- app/Http/Controllers/SalonController.php
+++ app/Http/Controllers/SalonController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Salon;
 
+use App\classes\Util;
+use Illuminate\Support\Facades\Log;
+
 class SalonController extends Controller
 {
     //
 
     public function index(){
-
+        Log::info(__METHOD__.'('.__LINE__.') start by user(' . Util::getUserId() .')');
+        Log::info(__METHOD__.'('.__LINE__.') end by user(' . Util::getUserId() .')');
     }
 
     public function show($salon_id){
