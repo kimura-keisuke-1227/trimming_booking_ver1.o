c303b9d0d378078c7d28bce33e34d01f64cb6614
Keisuke Kimura <kim.ksuke@gmail.com>
2023-03-01 19:05:51 +0900

    更新用の関数をとりあえず

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Setting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use App\classes\Util;
 
 class SettingController extends Controller
 {
@@ -17,4 +19,11 @@ class SettingController extends Controller
             'settings' => $settings
         ]);
     }
+
+    public function update(){
+        Log::info(__METHOD__.'('.__LINE__.') start by user(' . Util::getUserId() .')');
+        Log::info(__METHOD__.'('.__LINE__.') end by user(' . Util::getUserId() .')');
+        return __METHOD__;
+    }
+
 }
