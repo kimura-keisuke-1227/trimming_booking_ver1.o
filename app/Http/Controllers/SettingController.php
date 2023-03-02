52221856682738d62233fd53614c94db60435acc
Keisuke Kimura <kim.ksuke@gmail.com>
2023-03-02 11:23:08 +0900

    設定データを1つずつ読み込み

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -27,6 +27,20 @@ class SettingController extends Controller
     public function update(Request $request)
     {
         Log::info(__METHOD__ . '(' . __LINE__ . ') start by user(' . Util::getUserId() . ')');
+        
+        $setting_list = session('settingList');
+
+        foreach($setting_list as $setting){
+            Log::debug(__METHOD__.'('.__LINE__.') user(' . Util::getUserId() .') setting:id=' . $setting->id . ' setting:setting_name:' . $setting->setting_name);
+            $now_setting = Setting::find($setting->id);
+            if($now_setting->isNumber==1){
+                Log::debug(__METHOD__.'('.__LINE__.') user(' . Util::getUserId() .') This setting is numeric.');
+            } else{
+                Log::debug(__METHOD__.'('.__LINE__.') user(' . Util::getUserId() .') This setting is numeric.');
+
+            }
+        }
+        
         Log::info(__METHOD__ . '(' . __LINE__ . ') end by user(' . Util::getUserId() . ')');
         return __METHOD__;
     }
