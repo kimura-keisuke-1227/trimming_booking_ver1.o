076a26763af4ad5a8836534311c1e2afeabf9346
Keisuke Kimura <kim.ksuke@gmail.com>
2023-03-02 13:49:48 +0900

    全体設定の更新が成功

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -6,6 +6,7 @@ use App\Models\Setting;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\classes\Util;
+use Exception;
 use Illuminate\Support\Facades\Redis;
 
 class SettingController extends Controller
@@ -45,19 +46,29 @@ class SettingController extends Controller
                 Log::debug(__METHOD__.'('.__LINE__.') user(' . Util::getUserId() .') This setting is numeric.');
                 $modified = ($now_setting->setting_int<>$request->get('setting-' . $now_setting->id));
                 Log::debug(__METHOD__.'('.__LINE__.') user(' . Util::getUserId() .') modified:' . $modified);
+                $now_setting->setting_int = $request->get('setting-' . $now_setting->id);
             } else{
                 Log::debug(__METHOD__.'('.__LINE__.') user(' . Util::getUserId() .') This setting is not numeric.');
                 $modified = ($now_setting->setting_string<>$request->get('setting-' . $now_setting->id));
                 Log::debug(__METHOD__.'('.__LINE__.') user(' . Util::getUserId() .') modified:' . $modified);
-
+                $now_setting->setting_string = $request->get('setting-' . $now_setting->id);
             }
+
+
+            //modifiedの場合のみ更新
             if($modified){
-                //modifiedの場合のみ更新
-                Log::debug(__METHOD__.'('.__LINE__.') user(' . Util::getUserId() .') update!');
+                try{
+                    $now_setting->save();
+                    Log::notice(__METHOD__ . ' staff:user_id(' .Util::getUserId() . ') saved setting-id:' . $now_setting->id);
+                }catch(Exception $e){
+                    Log::error($e);
+                    return 'エラーが発生しました。お手数ですが直接店舗にお電話ください。';
+                }    
             }
         }
         
         Log::info(__METHOD__ . '(' . __LINE__ . ') end by user(' . Util::getUserId() . ')');
-        return __METHOD__;
+        return redirect()->route('admin.setting')
+        ->with("success", "設定を変更しました。");;
     }
 }
