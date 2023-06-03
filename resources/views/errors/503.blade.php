0a972a744d2884c3c54897f8774f199423a4538f
Keisuke Kimura <kim.ksuke@gmail.com>
2023-06-04 01:19:46 +0900

    時間の表示

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>メンテナンス中</title>
-    </head>
-    <body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>メンテナンス中</title>
+</head>
+<body>
         <h1>只今メンテナンス中です。</h1>
         <p>6月3日　21時から24時の間の予定です。</p>
     </body>
 </html>
-
 <!-- php artisan down --render="errors::503" -->
