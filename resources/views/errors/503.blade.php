521ce3ef20df4675f9017cbdfacd56dc2f45092c
Keisuke Kimura <kim.ksuke@gmail.com>
2023-06-03 23:45:22 +0900

    メンテナンス時間の表示

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>メンテナンス中</title>
+    </head>
+    <body>
+        <h1>只今メンテナンス中です。</h1>
+        <p>6月3日　21時から24時の間の予定です。</p>
+    </body>
+</html>
+
+<!-- php artisan down --render="errors::503" -->
