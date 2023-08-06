cb724ef7c9c146632be1a790cc30b42f3454ac3f
Keisuke Kimura <kim.ksuke@gmail.com>
2023-08-06 22:57:49 +0900

    メンテナンスメッセージ削除

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -60,7 +60,6 @@
       <div class="container px-4 mx-auto">
         <div class="flex max-w-md mx-auto flex-col text-center">
           <div class="mt-12 mb-8 p-8 bg-white rounded shadow">
-            <h1 class="mb-6 text-3xl">本日23時よりメンテナンスを行います。</h1>
             <h1 class="mb-6 text-3xl">{{$salon_name}}<br>予約システム</h1>
             <a href="{{ route('admin.users.create')}}" class="new_entry">新規登録はこちらから</a>
             <br><br>
