d713958c1b0671d7b2e521f4a33c0bf84d627e02
Keisuke Kimura <kim.ksuke@gmail.com>
2023-08-13 06:45:25 +0900

    Notificationコントローラー呼び出し成功

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\SalonController;
 use App\Http\Controllers\SettingController;
+use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\NonMemberBookingController;
 use App\Http\Controllers\PasswordController;
 use App\Http\Controllers\DefaultCapacityController;
@@ -312,6 +313,15 @@ Route::post('/admin/createStaff',[UserController::class,'storeStaff'])
 Route::get('/admin/showNonMember/{bookingId}',[BookingController::class,'showNonMember'])
 ->name('admin.showNonMemberInfo')
 -> middleware('auth');
+
+Route::resource('/admin/notification',
+ NotificationController::class
+)
+// -> only(['index','create','store','edit','update'])
+-> Middleware('auth')
+;
+
+
 /*****************************************************************
 *
 *   パスワードリセット
