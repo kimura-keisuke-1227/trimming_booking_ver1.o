fa6ad0f1556f11eac459965f159d6bd139cc111c
Keisuke Kimura <kim.ksuke@gmail.com>
2023-08-13 06:49:22 +0900

    お知らせ一覧表示用ページ

---
+++ resources/views/admin/notifications/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.admin')
+
+@section('title','サロン一覧')
+
+@section('content')
+
+<div class="container">
+    <table class="table table-striped pc_only">
+        <tr>
+            <th>サロン名</th>
+            <th>設定変更</th>
+        </tr>
+        @foreach($salons as $salon)
+        <tr>
+            <td>{{$salon -> salon_name}}</td>
+            <td><a href="{{ Route('admin.salon.edit',['salon_id' => $salon])}}">[設定]</a></td>
+        </tr>
+        @endforeach
+
+    </table>
+
+    <table class="table table-striped sp_only">
+        <tr>
+            <th>サロン一覧</th>
+        </tr>
+        @foreach($salons as $salon)
+        <tr>
+            <td><a href="{{ Route('admin.salon.edit',['salon_id' => $salon])}}">{{$salon -> salon_name}}</a></td>
+        </tr>
+        @endforeach
+    </table>
+    <a href="{{ Route('admin.salon.create')}}">サロン登録へ</a>
+
+</div>
+@endsection
+
+@section('footer')
+@endsection
