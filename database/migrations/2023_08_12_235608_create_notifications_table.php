6d726a5e78103c2fe19336a0214be24aa44274ac
Keisuke Kimura <kim.ksuke@gmail.com>
2023-08-13 00:03:17 +0900

    マイグレーションファイル

--- database/migrations/2023_08_12_235608_create_notifications_table.php
+++ database/migrations/2023_08_12_235608_create_notifications_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
+            $table->text('contents');
+            $table->foreignId('salon_id');
+            $table->string('page');
+            $table->timestamp('start_time');
+            $table->timestamp('end_time');
             $table->timestamps();
         });
     }
