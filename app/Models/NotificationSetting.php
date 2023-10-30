04a4d8f143608be6f3beb08e53c6cdb72f81d829
Keisuke Kimura <kim.ksuke@gmail.com>
2023-10-30 15:45:55 +0900

    モデルを定義

--- app/Models/NotificationSetting.php
+++ app/Models/NotificationSetting.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class NotificationSetting extends Model
 {
     use HasFactory;
+
+    public function notification(){
+        return $this -> belongsTo('App\Models\Notification');
+    }
 }
