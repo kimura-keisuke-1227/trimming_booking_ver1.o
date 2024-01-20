3c27ed5ec24147d411181798ac35fbebbc2d89f3
Keisuke Kimura <kim.ksuke@gmail.com>
2024-01-21 00:19:23 +0900

    とりあえずコースの時間一覧が表示できるようになった

--- resources/views/admin/courses/index.blade.php
+++ resources/views/admin/courses/index.blade.php
@@ -14,9 +14,9 @@
         </tr>
         @foreach ($courses as $course)
         <tr>
-            <td>{{$course->minute}}</td>
-            <td>{{$course->minute}}</td>
-            <td>{{$course->minute_for_show}}</td>
+            <td>{{$course->dogtype->type}}</td>
+            <td><input id="minute_{{ $course->id }}" class="block w-half px-4 py-3 mb-2 text-sm bg-white border rounded" type="number" name="minute_{{ $course->id }}"  value="{{$course->minute}}" min="0"></td>
+            <td><input id="minute_for_show_{{ $course->id }}" class="block w-half px-4 py-3 mb-2 text-sm bg-white border rounded" type="number" name="minute_for_show{{ $course->id }}"  value="{{$course->minute_for_show}}" min="0"></td>
         </tr>
         @endforeach
         
