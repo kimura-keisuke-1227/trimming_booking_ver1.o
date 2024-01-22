ced3c95da17d2ebfdb915eaab54de9b1428d1f9f
Keisuke Kimura <kim.ksuke@gmail.com>
2024-01-22 14:12:29 +0900

    エラーの表示変更

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -172,11 +172,12 @@
                     <!-- ▼▼▼▼エラーメッセージ(全ページで共通)▼▼▼▼　-->
                 --}}
                 <div class="mb-4 text-right">
-                    <div class="pl-6 pr-16 py-4 bg-white border-l-4 border-green-500 shadow-md rounded-r-lg inline-block ml-auto">
+                    <div class="pl-6 pr-16 py-4 bg-white border-l-4 border-red-500 shadow-md rounded-r-lg inline-block ml-auto">
                         <div class="flex items-center">
                             <span class="inline-block mr-2">
-                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M10 0C4.5 0 0 4.5 0 10C0 15.5 4.5 20 10 20C15.5 20 20 15.5 20 10C20 4.5 15.5 0 10 0ZM14.2 8.3L9.4 13.1C9 13.5 8.4 13.5 8 13.1L5.8 10.9C5.4 10.5 5.4 9.9 5.8 9.5C6.2 9.1 6.8 9.1 7.2 9.5L8.7 11L12.8 6.9C13.2 6.5 13.8 6.5 14.2 6.9C14.6 7.3 14.6 7.9 14.2 8.3Z" fill="#17BB84"></path>
+                                <svg width="40" height="40" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                                    <circle cx="20" cy="20" r="20" fill="#FF0000"/>
+                                    <path d="M14 14L26 26M26 14L14 26" stroke="white" stroke-width="4"/>
                                 </svg>
                             </span>
                             <p class="text-green-800 font-medium">{{ session('error') }}</p>
