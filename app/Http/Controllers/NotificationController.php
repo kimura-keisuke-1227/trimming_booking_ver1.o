2dfa66e006d6542bafbdb77e1b122b613b01cf8d
Keisuke Kimura <kim.ksuke@gmail.com>
2024-04-07 00:08:58 +0900

    保存後にお知らせ一覧にリダイレクト

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -56,6 +56,8 @@ class NotificationController extends Controller
         Log::info(__METHOD__.'('.__LINE__.') start by user(' . Util::getUserId() .')');
 
         Log::info(__METHOD__.'('.__LINE__.') end by user(' . Util::getUserId() .')');
+        return redirect()->route('notification.index')
+        ->with("success", "【まだ】お知らせを保存しました。");
     }
 
     /**
