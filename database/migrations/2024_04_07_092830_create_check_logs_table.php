300ef3fde5b755236527d540e879312de6cce521
Keisuke Kimura <kim.ksuke@gmail.com>
2024-04-07 09:45:12 +0900

    use文エラー修正

--- database/migrations/2024_04_07_092830_create_check_logs_table.php
+++ database/migrations/2024_04_07_092830_create_check_logs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-use app\Models\CheckLog;
+use App\Models\CheckLog;
 
 return new class extends Migration
 {
