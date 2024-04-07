5447730106f70accdf0d1c3dc8f8f6436d04f64e
Keisuke Kimura <kim.ksuke@gmail.com>
2024-04-07 09:37:47 +0900

    マイグレーションファイル途中

--- app/Models/CheckLog.php
+++ app/Models/CheckLog.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class CheckLog extends Model
 {
     use HasFactory;
+    const STR_TABLE_NAME_OF_CHECK_LOGS = 'check_logs';
+
+    const STR_COLUMN_NAME_OF_USER_INFO = 'user_info';
+    const STR_COLUMN_NAME_OF_SUMMARY = 'summary';
+    const STR_COLUMN_NAME_OF_DETAIL = 'detail';
 }
--- database/migrations/2024_04_07_092830_create_check_logs_table.php
+++ database/migrations/2024_04_07_092830_create_check_logs_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use app\Models\CheckLog;
+
 return new class extends Migration
 {
     /**
@@ -11,8 +13,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('check_logs', function (Blueprint $table) {
+        Schema::create(CheckLog::STR_TABLE_NAME_OF_CHECK_LOGS, function (Blueprint $table) {
             $table->id();
+            $table->string(CheckLog::STR_COLUMN_NAME_OF_USER_INFO);
+            $table->string(CheckLog::STR_COLUMN_NAME_OF_SUMMARY);
+            $table->string(CheckLog::STR_COLUMN_NAME_OF_DETAIL);
             $table->timestamps();
         });
     }
@@ -22,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('check_logs');
+        Schema::dropIfExists(CheckLog::STR_TABLE_NAME_OF_CHECK_LOGS);
     }
 };
