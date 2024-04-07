a93407d40565a867012ee11133ecf8fa1eede076
Keisuke Kimura <kim.ksuke@gmail.com>
2024-04-07 14:52:50 +0900

    ユーザーIDとメソッドを個別に

--- app/Models/CheckLog.php
+++ app/Models/CheckLog.php
@@ -11,12 +11,15 @@ class CheckLog extends Model
     const STR_TABLE_NAME_OF_CHECK_LOGS = 'check_logs';
 
     const STR_COLUMN_NAME_OF_USER_INFO = 'user_info';
+    const STR_COLUMN_NAME_OF_METHOD = 'method';
+    const STR_COLUMN_NAME_OF_USER_ID = 'user_id';
     const STR_COLUMN_NAME_OF_SUMMARY = 'summary';
     const STR_COLUMN_NAME_OF_DETAIL = 'detail';
     const STR_COLUMN_NAME_OF_REQUEST = 'request';
 
     const INT_LENGTH_OF_SUMMARY = 255;
     const INT_LENGTH_OF_USER_INFO = 255;
+    const INT_LENGTH_OF_USER_METHOD = 255;
 
     protected $fillable = [
         'user_info','summary','detail','request'
