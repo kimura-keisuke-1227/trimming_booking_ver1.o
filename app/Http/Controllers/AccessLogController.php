ad38b80e3d2d898f43b6c9d1b462052711508064
Keisuke Kimura <kim.ksuke@gmail.com>
2024-04-08 15:41:37 +0900

    アクセスログ一覧表示

--- app/Http/Controllers/AccessLogController.php
+++ app/Http/Controllers/AccessLogController.php
@@ -8,14 +8,36 @@ use App\Models\AccessLog;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
 
+use Illuminate\Support\Facades\Log;
+use App\classes\Util;
+use Illuminate\Support\Facades\Auth;
+
 class AccessLogController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): Response
+    public function index()
     {
-        //
+        Log::info(__METHOD__.'('.__LINE__.')'.'start!');
+        // 操作記録をDBに
+        $user =Auth::user();
+        $method_name = __METHOD__;
+        $realIp = request()->ip();
+
+        $user_info = "user_id({$user->id}) IP[{$realIp}]";
+        $check_log_summary = "アクセスログの一覧を取得[{$method_name}]";
+        $check_log_detail = "";
+        $request_from_user = request();
+        $access_log_id = Util::recordAccessLog(__METHOD__,$user_info,$check_log_summary,$check_log_detail,$request_from_user);
+
+        $access_log_list = AccessLog::query()
+        ->orderBy('created_at','desc')
+        ->get();
+        Log::info(__METHOD__.'('.__LINE__.')'.'end!');
+        return view('admin.accessLog.index',[
+            'list_accessLog' => $access_log_list,
+        ]);
     }
 
     /**
