05d791ca7044f1db1f118e8e71a32e19fbf34267
Keisuke Kimura <kim.ksuke@gmail.com>
2024-05-08 03:10:59 +0900

    googleタグをDBにから習得

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -16,6 +16,11 @@
     <script src="{{ asset('/js/admin/select2.min.js')}}"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('/css/style.css')}}" >
+    @php
+        use app\classes\Util;
+        $google_Tag = Util::getSetting("hoge", "google_tag", false);
+        echo($google_Tag);
+    @endphp
 </head>
 
 <body class="antialiased bg-body text-body font-body">
