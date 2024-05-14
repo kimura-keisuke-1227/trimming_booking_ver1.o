6e527a9aa1ff589a7e4e28a018561bb72315e967
Keisuke Kimura <kim.ksuke@gmail.com>
2024-05-14 22:49:45 +0900

    ボタン変更

--- resources/views/bookings/cancelConfirm.blade.php
+++ resources/views/bookings/cancelConfirm.blade.php
@@ -17,7 +17,9 @@
         ])}}" method="post">
         @csrf
         @method('DELETE')
-        <input type="submit" name="" id="" value="キャンセルする">
+        <div class="ml-auto">
+            <button type="submit" class="py-2 px-3 text-xs text-white font-semibold bg-indigo-500 rounded-md">キャンセルする</button>
+        </div>
     </form>
 </div>
 @endsection
