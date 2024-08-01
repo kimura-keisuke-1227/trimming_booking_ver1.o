4403279a29131ff504d5816cd48b3169b60790d7
Keisuke Kimura <kim.ksuke@gmail.com>
2024-08-01 16:50:46 +0900

    休日作成

---
+++ app/Models/Holiday.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Holiday extends Model
+{
+    const CONST_STR_TABLE_NAME_OF_HOLIDAYS = 'holidays';
+
+    const CONST_STR_COLUMN_NAME_OF_SALON_ID = 'salon_id';sai
+    const CONST_STR_COLUMN_NAME_OF_DATE = 'date';
+
+    public function salon(){
+        return $this -> belongsTo('App\Models\salon');
+    }
+    use HasFactory;
+}
---
+++ database/migrations/2024_08_01_164018_create_holidays_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use App\Models\Holiday;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(Holiday::CONST_STR_TABLE_NAME_OF_HOLIDAYS, function (Blueprint $table) {
+            $table->id();
+            $table->foreignId(Holiday::CONST_STR_COLUMN_NAME_OF_SALON_ID);
+            $table->foreignId(Holiday::CONST_STR_COLUMN_NAME_OF_DATE);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(Holiday::CONST_STR_TABLE_NAME_OF_HOLIDAYS);
+    }
+};
