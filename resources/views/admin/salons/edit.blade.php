0194fad6c31ef5ddfda6aace16e40c4ed8d5d31f
Keisuke Kimura <kim.ksuke@gmail.com>
2024-08-01 21:19:07 +0900

    全コースクローズ選択肢追加

--- resources/views/admin/salons/edit.blade.php
+++ resources/views/admin/salons/edit.blade.php
@@ -61,6 +61,28 @@
                             <input id="close_h" class="block w-full px-4 py-3 mb-2 text-sm bg-white border rounded" type="text" name="ed_time" value="{{$close}}">
                         </div>
                     --}}
+                    <div class="mb-6">
+                        <label class="block text-sm font-medium mb-2" for="salon">全コース予約枠自動クローズ</label>
+                        <div class="flex">
+                            <select id="salon" class="appearance-none block pl-4 pr-8 py-3 mb-2 text-sm bg-white border rounded" name="is_close_all_courses">
+                                <option value="0">
+                                    予約コースのみ
+                                </option>
+                                <option value="1" 
+                                @if ($salon->is_close_all_courses)
+                                    selected
+                                @endif>
+                                    全コース
+                                </option>
+                            </select>
+                            <div class="pointer-events-none transform -translate-x-full flex items-center px-2 text-gray-500">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20">
+                                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"></path>
+                                </svg>
+                            </div>
+                        </div>
+                        <br><br>
+                    </div>
                     <div class="ml-auto">
                         <button type="submit" class="py-2 px-3 text-xs text-white font-semibold bg-indigo-500 rounded-md">変更</button>
                     </div>
