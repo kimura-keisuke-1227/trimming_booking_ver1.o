7fc5c123d85ac722ccbe2062bde24b9bdda22694
Keisuke Kimura <kim.ksuke@gmail.com>
2024-08-08 23:47:42 +0900

    単一休日の保存時リクエスト

--- app/Http/Requests/SingleHolidayRqeuest.php
+++ app/Http/Requests/SingleHolidayRqeuest.php
@@ -11,7 +11,7 @@ class SingleHolidayRqeuest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class SingleHolidayRqeuest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'single_date' => 'required|date',
+            'single_comment' => 'required|string',
         ];
     }
 }
