2151b5e97f21bbe905fb6043532501c227a38256
Keisuke Kimura <kim.ksuke@gmail.com>
2024-08-09 07:45:43 +0900

    複数のリクエスト追加

--- app/Http/Requests/MultipleHolidayRqeuest.php
+++ app/Http/Requests/MultipleHolidayRqeuest.php
@@ -11,7 +11,7 @@ class MultipleHolidayRqeuest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class MultipleHolidayRqeuest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'st_date' => 'required|date',
+            'ed_date' => 'required|date',
+            'single_comment' => 'required|string',
         ];
     }
 }
