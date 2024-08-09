c73fc17a4444165c19c839b72c342fedaab4e8dd
Keisuke Kimura <kim.ksuke@gmail.com>
2024-08-09 12:52:25 +0900

    店舗ごとの設定画面を開けるように

--- resources/views/admin/holiday/index.blade.php
+++ resources/views/admin/holiday/index.blade.php
@@ -5,26 +5,19 @@
 @section('content')
 
 <div class="container">
-        <div class="mb-6 ">
-            <label class="block text-sm font-medium mb-2" for="salon">店舗を選択してください。</label>
-            <div class="flex">
-                <select id="salon" class="appearance-none block pl-4 pr-8 py-3 mb-2 text-sm bg-white border rounded" name="salon">
-                    @foreach($salons as $salon)
-                        <option value="{{$salon -> id}}" 
-                            @if( $salon->id == $salon_id)
-                                selected
-                            @endif
-                            >{{$salon -> salon_name}}</option>
-                    @endforeach
-                </select>
-                <div class="pointer-events-none transform -translate-x-full flex items-center px-2 text-gray-500">
-                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20">
-                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"></path>
-                    </svg>
-                </div>
-            </div>
-            <br><br>
-        </div>
+        <div class="py-4 bg-white rounded">
+        <table class="table table-striped pc_only">
+
+        @foreach($salons as $salon)
+        <tr>
+            <td>{{$salon -> salon_name}}</td>
+            <td><a href="{{Route('admin.holiday',['salon_id' => $salon->id])}}">[設定を開く]</a></td>
+        </tr>
+        @endforeach
+
+    </table>
+
+    </div>
         <table class="table table-striped pc_only">
         <tr>
             <!-- <th>id</th> -->
