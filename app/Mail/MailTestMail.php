a7ba717859e444291a47964d2bcfa3cbd81d6aca
Keisuke Kimura <kim.ksuke@gmail.com>
2024-08-09 17:00:55 +0900

    サロンへのメールテストクラス

---
+++ app/Mail/MailTestMail.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+use App\classes\Util;
+
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+class MailTest extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        // $user = Auth::user();
+        // $date = session('date');
+        // $st_time = session('time');
+        // $cut_time = session('course')->minute;
+        // $cut_time_for_show = session('course')->minute_for_show;
+        // $ed_time = $st_time + $cut_time;
+        // $ed_time_for_show = $st_time + $cut_time_for_show;
+        // $pet = session('pet');
+        // $course = session('course');
+        // $price =  session('course')->price;
+        // $salon = session('salon')->id;
+        // $booking_status = 1;
+        // $message = session('message');
+
+        // Log::debug(__METHOD__ . ' message：' . $message);
+
+        // $salon = session('salon');
+
+        $mailFrom = 'support@conaffetto-saitama.com';
+        // $mailFrom = $salon -> email;
+        $mailFrom = Util::getSetting($mailFrom,'mailFromSalon',false);
+
+        return $this->from($mailFrom) 
+        ->subject('予約を受付けました。')
+        ->text('email.bookingNotification.bookingNotificationToStaff',[
+        ]);
+    }
+}
