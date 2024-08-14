3bdf46b0ef53c2fa3b03581bfccdfdc5417cf58b
Keisuke Kimura <kim.ksuke@gmail.com>
2024-08-14 21:31:21 +0900

    登録用のルート

--- resources/views/admin/dogtypes/create.blade.php
+++ resources/views/admin/dogtypes/create.blade.php
@@ -6,7 +6,7 @@
 <section class="py-8">
     <div class="container px-4 mx-auto">
         <div class="py-4 bg-white rounded">
-            <form action="{{Route('pets.store')}}" method="post" enctype="multipart/form-data">
+            <form action="{{Route('dogtype.store')}}" method="post" enctype="multipart/form-data">
                 @csrf
                 <div class="flex px-6 pb-4 border-b">
                     <h3 class="text-xl font-bold">犬種登録</h3>
