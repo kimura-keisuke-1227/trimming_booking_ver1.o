4a1087fa376fa46bd1819eda1f2cfd3e8a780b79
Keisuke Kimura <kim.ksuke@gmail.com>
2024-08-14 21:21:32 +0900

    一覧画面

--- resources/views/admin/dogtypes/index.blade.php
+++ resources/views/admin/dogtypes/index.blade.php
@@ -2,22 +2,27 @@
 
 @section('content')
 <div class="container">
-    <form action="{{Route('admin.course.store')}}" method="post" enctype="multipart/form-data">
-        @csrf
         <table class="table table-striped">
             <tr>
                 <th>犬種</th>
                 <th>有効・無効</th>
-                <th></th>
+                <th>表示切替</th>
             </tr>
             @foreach ($dogtypes as $dogtype)
             <tr>
                 <td>{{$dogtype->type}}</td>
-                <td><a href="">[無効化]</a></td>
-                <td><a href="">[無効化]</a></td>
+                <td>@if ($dogtype->flg_show)
+                    有効
+                @else
+                    ×
+                @endif</td>
+                <td>@if ($dogtype->flg_show)
+                    <a href="">[無効化]</a>
+                @else
+                    <a href="">[有効化]</a>
+                @endif</td>
             </tr>
             @endforeach
-        </table>
     </form>
 
 </div>
