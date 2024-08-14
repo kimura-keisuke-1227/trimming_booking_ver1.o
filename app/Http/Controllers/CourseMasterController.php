f4fde8e1d7c785d26ceebb091196b95fa73432f9
Keisuke Kimura <kim.ksuke@gmail.com>
2024-08-15 00:10:56 +0900

    とりあえず編集画面

--- app/Http/Controllers/CourseMasterController.php
+++ app/Http/Controllers/CourseMasterController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use App\classes\Util;
 use Exception;
 
+use App\Models\Course;
 use App\Models\CourseMaster;
+use App\Models\Dogtype;
 
 class CourseMasterController extends Controller
 {
@@ -40,9 +43,69 @@ class CourseMasterController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request)
     {
-        //
+        Log::info(__METHOD__ . '(' . __LINE__ . ')' . ' start!');
+
+        // 操作記録をDBに
+        $user =Auth::user();
+        $method_name = __METHOD__;
+        $realIp = request()->ip();
+
+        $course = $request['course'];
+
+        $user_info = "user_id({$user->id}) IP[{$realIp}]";
+        $check_log_summary = "基本コースの登録";
+        $check_log_detail = "基本コース:{$course}";
+        $access_log_id = Util::recordAccessLog(__METHOD__,$user_info,$check_log_summary,$check_log_detail,$request);
+        
+        $course_master_max_order = CourseMaster::query()
+        ->max('order');
+        ;
+
+        $new_order = $course_master_max_order + 10;
+
+        $course_master = new CourseMaster();
+
+        $course_master['course'] = $course;
+        $course_master['order']  = $new_order;
+
+        try{
+            $course_master->save();
+            Log::info(__METHOD__ . '(' . __LINE__ . ')' . "Successfully_create_a_new_course_master:" . $course .' order:' . $new_order);
+        }
+        catch(Exception $e){
+            Log::error(__METHOD__ . '(' . __LINE__ . ')' . 'Error_occurred_when_create_course_master:' . $course .' ' . $e);
+            Log::error(__METHOD__ . '(' . __LINE__ . ')' . ' end!');
+            return redirect(Route('admin.course_master.index'))
+                ->with('error','基本コースを登録に失敗しました。');
+        }
+
+        $dogTypes = Dogtype::all();
+
+        // すべての犬種に対して、コースを設定。ただし、最初は非表示。
+        foreach($dogTypes as $dogType){
+            $course = new Course();
+            $course['course_master_id'] = $course_master->id;
+            $course['dogtype_id'] = $dogType->id;
+            $course['minute'] = 60*24;
+            $course['minute_for_show'] = 60*24;
+            $course['price'] = 99999;
+            $course['flg_show'] = false;
+            $course['st_date'] = "2020-01-01";
+
+            try{
+                $course->save();
+                Log::info(__METHOD__ . '(' . __LINE__ . ')' . 'Successfully_create_a_new_course_when_create_a_course_master.');
+            } catch(Exception $e){                
+                Log::error(__METHOD__ . '(' . __LINE__ . ')' . 'Error_occurred_when_create_a_new_course_when_create_a_course_master. ' . $e);
+            }
+
+        }
+
+        Log::info(__METHOD__ . '(' . __LINE__ . ')' . ' end!');
+        return redirect(Route('admin.course.edit'))
+            ->with('success','基本コースを登録しました。コース情報を確認してください。');
     }
 
     /**
@@ -58,7 +121,9 @@ class CourseMasterController extends Controller
      */
     public function edit(string $id): Response
     {
-        //
+        Log::info(__METHOD__ . '(' . __LINE__ . ')' . ' start!');
+        Log::info(__METHOD__ . '(' . __LINE__ . ')' . ' end!');
+        return __METHOD__;
     }
 
     /**
